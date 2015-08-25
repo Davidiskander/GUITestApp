077e8329a6ff8b0a0f7b4363f7aaa6bd7cc9957d
Davidiskander <david.iskander.di@gmail.com>
2015-08-25 11:49:19 -0700

    Delete settings_local.py

--- settings_local.py
+++
@@ -1,7 +0,0 @@
-__author__ = 'davidiskander'
-
-
-APPLICATION_ID = "nOG7Y95wdj3twZqa5mtF3Pe1lcbMnOppQ8IQjXxu"
-REST_API_KEY = "PQw0bhDYATBkQgqydOfF3rF7PvfLzYaSuhzSXM4k"
-MASTER_KEY = "ZzA5GBCQwy1btnOGbdNW13IGYj56V39dM9tBi2Tg"
-
